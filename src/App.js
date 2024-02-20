196567cf453ed28141debe639afe933342132f92
walter liendo <walter.liendo@gmail.com>
2024-02-20 01:41:58 -0300

    inclui firestore

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Navbar from "./components/Navbar"
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import { CartProvider } from './Context/context';
 
 
 const App = () => {
   return (
-    <>    <BrowserRouter>
-        <Navbar />
-        <Routes >
-            <Route path="/" element={<LandingPage />} />
-        </Routes>
-    </BrowserRouter>
+    <>   
+    <CartProvider>
+        <BrowserRouter>
+            <Navbar />
+            <Routes >
+                <Route path="/" element={<LandingPage />} />
+            </Routes>
+        </BrowserRouter>
+    </CartProvider>
     </>
 
   );
